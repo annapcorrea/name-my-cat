ab70533643df886c1fc63c154908a2b52f153ef1
annapcorrea <annacorrea.us@gmail.com>
2025-03-14 01:52:58 -0400

    Fix DATABASE_URL parsing and add Postgres driver

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///cat_name.db')  # fallback to sqlite for local
+    uri = os.getenv("DATABASE_URL")  # get DATABASE_URL
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')  # use an env var or fallback
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
