3a0b37fbc4c446d65607a6bb197413e54a7003c6
annapcorrea <annacorrea.us@gmail.com>
2025-03-19 16:09:02 -0400

    Update config and requirements

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
         uri = uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_flaKEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
