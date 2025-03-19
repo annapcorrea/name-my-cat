a3254c8a9a1bfd2cdf411cdc6cc6917d43fd7e60
annapcorrea <annacorrea.us@gmail.com>
2025-03-19 15:05:29 -0400

    Add cat_name table and apply migrations

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    uri = os.getenv("DATABASE_URL")  # get DATABASE_URL
-    if uri and uri.startswith("postgres://"):
+    uri = os.getenv("DATABASE_URL", "sqlite:///default.db")  # Default to SQLite
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_flaKEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
